516fbf3686ceed755f72ee5c401861c62d82e952
H. Bappi <53597251+hbappi@users.noreply.github.com>
2023-05-19 06:05:21 +0600

    Update style-helper.tsx

--- style-helper.tsx
+++ style-helper.tsx
@@ -3,6 +3,23 @@ import { CSSProperties } from "react";
 
 type CSSSuggestionType = CSSProperties | SxProps<Theme>;
 
+/* TODO:
+    don't forget to follow me on:
+
+    github: https://github.com/hbappi
+    github: https://github.com/efortshub
+
+    facebook: https://facebook.com/h.bappi.hp
+    facebook: https://facebook.com/efortshub
+
+    linkedIn: https://www.linkedin.com/in/hbappi
+    LinkedIn: https://www.linkedin.com/company/efortshub
+    
+    instagram: https://www.instagram.com/efortshub
+    youtube: https://www.youtube.com/channel/@eFortsHub
+
+*/
+
 type _st =
   | (
       | CSSSuggestionType
